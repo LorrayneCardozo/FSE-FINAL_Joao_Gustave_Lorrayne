5ab58aba75a49a546f4f464e9c4f6453d41906a1
João Victor Batista <jvtexbat@hotmail.com>
2023-02-12 20:20:59 -0300

    Refact dos sensores para usar gpio setup

--- src/ky003.c
+++ src/ky003.c
@@ -4,34 +4,26 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_http_client.h"
-#include "esp_log.h"
 #include "freertos/semphr.h"
-#include "driver/gpio.h"
+#include "esp_adc/adc_oneshot.h"
+
+#include "gpio_setup.h"
+#include "nvs_handler.h"
 
 #define LED_PIN 2
 #define HALL_SENSOR_PIN 4
 
 void hall_sensor_task(void *pvParameter)
 {
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = (1ULL << LED_PIN);
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
-
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = (1ULL << HALL_SENSOR_PIN);
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
+    pinMode(HALL_SENSOR_PIN, GPIO_INPUT);
+    pinMode(LED_PIN, GPIO_OUTPUT);
 
     char mensagem[50];
 
     while (1)
     {
-        int state = gpio_get_level(HALL_SENSOR_PIN); // lê o estado do pino
+        int state = digitalRead(HALL_SENSOR_PIN); // lê o estado do pino
+
         printf("Hall Magnetic: %d\n", state);
         sprintf(mensagem, "{\"HallMagnetic\": %d}", state);
         mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
--- src/ky038.c
+++ src/ky038.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "esp_log.h"
+#include <string.h>
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "freertos/semphr.h"
+#include "esp_adc/adc_oneshot.h"
 
-#define KY038_PIN 4
+#include "gpio_setup.h"
+#include "nvs_handler.h"
+
+#define VOICE_SENSOR_PIN 4
 #define LED_PIN 2
 
 void voice_sensor_task(void *pvParameter)
 {
-    // Configura o pino do sensor de voz como entrada
-    esp_rom_gpio_pad_select_gpio(KY038_PIN);
-    gpio_set_direction(KY038_PIN, GPIO_MODE_INPUT);
-
-    // Configura o pino do LED como saída
-    esp_rom_gpio_pad_select_gpio(LED_PIN);
-    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    pinMode(HALL_SENSOR_PIN, GPIO_INPUT);
+    pinMode(LED_PIN, GPIO_OUTPUT);
 
     // Verifica se o sensor de voz foi acionado e liga o LED
     while (1)
     {
-        int level = gpio_get_level(KY038_PIN);
-        printf("SOUND: %d", level);
+        int level = digitalRead(VOICE_SENSOR_PIN);
+        printf("SOUND: %d\n", level);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-        if (level == 0)
-        {
-            gpio_set_level(LED_PIN, 1);
-        }
-        else
-        {
-            gpio_set_level(LED_PIN, 0);
-        }
+        if (level == 0) digitalWrite(LED_PIN, 1);
+        else digitalWrite(LED_PIN, 0);
     }
 }
